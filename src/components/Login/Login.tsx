aa17b3c08048443df4ad2789ca9bd87889e9ca65
Karol Zyśk <trener.karolzysk@gmail.com>
2022-05-11 21:09:18 +0200

    set up aplication for testing

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [error, setError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [user, setUser] = useState({} as User);
+
+  type User = {
+    name: string;
+  };
+
+  const handleClick = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then((response) => response.json())
+        .then((data) => setUser(data));
+    } catch {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="container">
+      <span className="user">{user.name}</span>
+      <form>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button disabled={!username || !password} onClick={handleClick}>
+          {loading ? "please wait" : "Login"}
+        </button>
+        <span
+          data-testid="error"
+          style={{ visibility: error ? "visible" : "hidden" }}
+        >
+          Something went wrong!
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
